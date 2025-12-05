225f412eaeaa73a0f06e6cf622c8970bab494c80
Sofia Shestakova <shestakova.sofia2016@mail.ru>
2025-12-05 10:07:25 +0400

    moved files

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+import DropdownModule from './modules/dropdown.js';
+import MobileMenu from './modules/mobileMenu.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  DropdownModule();
+  MobileMenu();
+
+
+  AOS.init();
+
+
+});
